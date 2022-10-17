2b019fad3102bbaace40d26daf6af68800ac93e4
alext <alekey.tereshkin@mail.ru>
2022-10-18 00:13:22 +0300

    add

--- Calculator/Form1.cs
+++ Calculator/Form1.cs
@@ -12,8 +12,6 @@ namespace Calculator
         public Form1()
         {
             InitializeComponent();
-      
-           
         }
 
 
@@ -24,14 +22,14 @@ namespace Calculator
                 firstNum = double.Parse(this.textBox1.Text);
                 secondNum = double.Parse(this.textBox2.Text);
                 DataNums data = new DataNums(firstNum,secondNum);
-               Form2 frm = new Form2(data);
+                Form2 frm = new Form2(data);
                 frm.Owner = this;
                 frm.Text = "Результат сложения";
                 frm.ShowDialog();
             }
             catch
             {
-                MessageBox.Show("Неверный вввод данных, повторите ввод данных");  
+                MessageBox.Show("Неправильные данные!");  
             }
             count++;
             this.label4.Text = count.ToString();
@@ -41,63 +39,58 @@ namespace Calculator
         {
             this.Text = "Терешкин А.А ЭПБ-211";
         }
-        void sec_MyDelegateEvent(string s)
-        {
-            label4.Text = s;
-        }
+     
 
         private void button2_Click(object sender, EventArgs e)
         {
+            Form2 frm = new Form2(null);
+            frm.Owner = this;
+            frm.Text = "Результат вычитания";
+        
+
             try
             {
                  firstNum = double.Parse(this.textBox1.Text);
                  secondNum = double.Parse(this.textBox2.Text);
-                DataNums data = new DataNums(firstNum, secondNum);
-                Form2 frm = new Form2(null);
-                frm.Owner = this;
-                frm.label1.Text = Math.Round(firstNum - secondNum, 2).ToString();
-                frm.Text = "Результат вычитания";
-                frm.ShowDialog();
-            
-               
-            
+                 frm.label1.Text = Math.Round(firstNum - secondNum, 2).ToString();
+                 frm.ShowDialog();
             }
             catch
             {
-                MessageBox.Show("Неверный вввод данных, повторите ввод данных");
+                MessageBox.Show("Неправильные данные!");
             }
           
         }
 
         private void button3_Click(object sender, EventArgs e)
         {
+            Form2 frm = new Form2(null);
+            frm.Owner = this;
+            frm.Text = "Результат возвдения в степень";
             try
             {
                 firstNum = double.Parse(this.textBox1.Text);
                 secondNum = double.Parse(this.textBox2.Text);
-                Form2 frm = new Form2(null);
-                frm.Owner = this;
-                frm.Text = "Результат возвдения в степень"; 
                 frm.label1.Text= Math.Pow(firstNum,secondNum).ToString();
                 frm.ShowDialog();
             }   
             catch
             {
-                MessageBox.Show("Неверный вввод данных, повторите ввод данных");
+                MessageBox.Show("Неправильные данные!");
             }
          
         }
 
         private void button4_Click(object sender, EventArgs e)
         {
+            Form2 frm = new Form2(null);
+            frm.Owner = this;
+            frm.Text = "Результат сравнения";
             try
             {
                 firstNum = double.Parse(this.textBox1.Text);
                 secondNum = double.Parse(this.textBox2.Text);
-                Form2 frm = new Form2(null);
-             
-                frm.Owner = this;
-                frm.Text = "Результат сравнения";
+              
                 if (firstNum > secondNum) frm.label1.Text = $"Большее число---> {firstNum}";
                 else if (firstNum < secondNum) frm.label1.Text = $"Большее число---> {secondNum}";
                 else frm.label1.Text = "Числа равны";
@@ -105,7 +98,7 @@ namespace Calculator
             }
             catch
             {
-                MessageBox.Show("Неверный вввод данных, повторите ввод данных");
+                MessageBox.Show("Неправильные данные!");
             }
          
         }
